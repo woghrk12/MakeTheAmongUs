f52021eb6029522493ed49db7672cd3eb3f1d9ac
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2021-11-07 14:10:30 +0900

    [기능] 배경에서 크루원이 날라가는 기능 추가

--- Assets/2Script/MainMenu/Background/CrewSpawner.cs
+++ Assets/2Script/MainMenu/Background/CrewSpawner.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class CrewSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Sprite[] sprites;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private GameObject crewPrefab;
+
+    [SerializeField] private Transform topRight;
+    [SerializeField] private Transform bottomLeft;
+
+    private void SpawnFloatingCrew(EPlayerColor color, float dist)
     {
-        
+        int angle = Random.Range(0, 360);
+
+        Vector3 spawnPos = new Vector3(Mathf.Sin(angle), Mathf.Cos(angle), 0f) * dist;
+        Vector3 direction = (new Vector3(
+                Random.Range(topRight.position.x, bottomLeft.position.x),
+                Random.Range(topRight.position.y, bottomLeft.position.y),
+                0f
+                ) - spawnPos).normalized;
+        float floatingSpeed = Random.Range(1f, 3f);
+        float rotateSpeed = Random.Range(-1.5f, 1.5f);
+
+        var crew = Instantiate(crewPrefab, spawnPos, Quaternion.identity).GetComponent<FloatingCrew>();
+        crew.SetFloatingCrew(
+            sprites[Random.Range(0, sprites.Length)],
+            direction,
+            floatingSpeed,
+            rotateSpeed,
+            Random.Range(0.8f, 1f),
+            color
+            );
     }
 }
