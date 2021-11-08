7948c71cf4d7d1686b40fdc65e6b21bef41b4ddf
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2021-11-08 13:16:18 +0900

    [기능] 최대 플레이어 설정 기능 추가

--- Assets/2Script/MainMenu/UI/CreateRoomUI.cs
+++ Assets/2Script/MainMenu/UI/CreateRoomUI.cs
@@ -1,16 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CreateRoomUI : MonoBehaviour
 {
     [SerializeField] private GameObject onlineUI;
 
+    [SerializeField] private List<Button> maxPlayerButtons;
+
+    private int maxPlayerCount = 10;
+
     public void OnClickCancelButton() => CloseCreateRoomUI();
+    public void OnClickMaxPlayerButton(int value) => UpdateMaxPlayerCount(value);
 
     private void CloseCreateRoomUI()
     {
         onlineUI.SetActive(true);
         gameObject.SetActive(false);
+    }
+
+    private void UpdateMaxPlayerCount(int value)
+    {
+        maxPlayerCount = value;
+
+        for (int i = 0; i < maxPlayerButtons.Count; i++)
+        {
+            SetButtonAlpha(maxPlayerButtons[i], (i == value - 4) ? 1f : 0f);
+        }
+    }
+
+    private void SetButtonAlpha(Button button, float alpha)
+    {
+        button.image.color = new Color(1f, 1f, 1f, alpha);
     }
 }
