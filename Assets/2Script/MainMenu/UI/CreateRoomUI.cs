1844068d395e9c028303f963393b3d6b00516488
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2021-11-08 13:59:10 +0900

    [기능] 임포스터 수 설정 기능 추가

--- Assets/2Script/MainMenu/UI/CreateRoomUI.cs
+++ Assets/2Script/MainMenu/UI/CreateRoomUI.cs
@@ -8,11 +8,14 @@ public class CreateRoomUI : MonoBehaviour
     [SerializeField] private GameObject onlineUI;
 
     [SerializeField] private List<Button> maxPlayerButtons;
+    [SerializeField] private List<Button> imposterButtons;
 
     private int maxPlayerCount = 10;
+    private int imposterCount = 1;
 
     public void OnClickCancelButton() => CloseCreateRoomUI();
     public void OnClickMaxPlayerButton(int value) => UpdateMaxPlayerCount(value);
+    public void OnClickImposterButton(int value) => UpdateImposterCount(value);
 
     private void CloseCreateRoomUI()
     {
@@ -27,6 +30,16 @@ public class CreateRoomUI : MonoBehaviour
         for (int i = 0; i < maxPlayerButtons.Count; i++)
         {
             SetButtonAlpha(maxPlayerButtons[i], (i == value - 4) ? 1f : 0f);
+        }
+    }
+
+    private void UpdateImposterCount(int value)
+    {
+        imposterCount = value;
+
+        for (int i = 0; i < imposterButtons.Count; i++)
+        {
+            SetButtonAlpha(imposterButtons[i], (i == value - 1) ? 1f : 0f);
         }
     }
 
