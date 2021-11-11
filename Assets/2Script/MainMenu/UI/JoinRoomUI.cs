79a122744fbc3321fa1def8f9212313b8b7d084a
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2021-11-11 18:59:29 +0900

    [기능] 일정 시간마다 현재 포톤에 존재하는 방 정보로 RoomListItem을 생성하는 기능 추가

--- Assets/2Script/MainMenu/UI/JoinRoomUI.cs
+++ Assets/2Script/MainMenu/UI/JoinRoomUI.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Realtime;
+
 
 public class JoinRoomUI : MonoBehaviour
 {
     [SerializeField] private GameObject onlineUI;
+    [SerializeField] private GameObject roomListPanel;
+    [SerializeField] private GameObject roomItemPrefab;
+
+    private void OnEnable()
+    {
+        StopCoroutine(RoomListItemUpdateCo());
+        StartCoroutine(RoomListItemUpdateCo());
+    }
 
     public void OnClickBackButton() => CloseJoinRoomUI();
 
@@ -12,5 +22,26 @@ public class JoinRoomUI : MonoBehaviour
     {
         onlineUI.SetActive(true);
         gameObject.SetActive(false);
+    }
+
+    private IEnumerator RoomListItemUpdateCo()
+    {
+        while (true)
+        {
+            RoomListItemUpdate(AmongUsNetworkManager.Instance.roomList);
+
+            yield return new WaitForSeconds(3f);
+        }
+    }
+
+    private void RoomListItemUpdate(List<RoomInfo> _roomList)
+    {
+        foreach (var room in _roomList)
+        {
+            var roomItem = Instantiate(roomItemPrefab, roomListPanel.transform);
+
+            // Set roominfo by customproperties of room
+            //roomItem.GetComponent<RoomListItem>().SetItem();
+        }
     }
 }
